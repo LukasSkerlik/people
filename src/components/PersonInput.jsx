3a4b37eee261068a7d01f1b167b0378ebdf34de7
Lukas Skerlik <skerlik.lukas@gmail.com>
2020-11-08 21:25:34 +0000

    implement Update and Delete of CRUD

--- src/components/PersonInput.jsx
+++ src/components/PersonInput.jsx
@@ -1,15 +1,26 @@
 import React from "react"
-
+import firebase from "../firebase"
 
 function PersonInput({person}) {
   const [firstName,setFirstName] = React.useState(person.fName);
   const [lastName,setLastName] = React.useState(person.lName);
 
+  const onUpdate = () => {
+    const db = firebase.firestore();
+    db.collection("people").doc(person.id).set({...person, fName:firstName, lName:lastName});
+  }
+  const onDelete = ()=>{
+    const db = firebase.firestore();
+    db.collection("people").doc(person.id).delete();
+
+  }
 
   return (<>
-            <input value= {firstName}></input>
-            <input value= {lastName}></input>
-            <br />
+            <input value= {firstName} onChange= {(e)=>{setFirstName(e.target.value)}}></input>
+            <input value= {lastName} onChange= {(e)=>{setLastName(e.target.value)}}></input>
+            <button onClick={onUpdate}>Update</button>
+            <button onClick={onDelete}>Delete</button>
+            <br></br>
           </>)
 }
 
